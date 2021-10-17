2ae4020f84806561762923eaee156f3d171b0efd
limitzhang <470299041@qq.com>
2021-10-17 19:23:45 +0800

    Day5 中间件

---
+++ Gee/05_middleware/gee/gee.go
@@ -0,0 +1,85 @@
+package gee
+
+import (
+	"net/http"
+	"strings"
+)
+
+// HandlerFunc defines the request handler used by gee
+type HandlerFunc func(c *Content)
+
+// Engine implement the interface of ServerHTTP
+type (
+	RouterGroup struct {
+		prefix      string
+		middlewares []HandlerFunc // support middleware
+		parent      *RouterGroup  // support nesting
+		engine      *Engine       // all group share a Engine instance
+	}
+	Engine struct {
+		*RouterGroup
+		router *router
+		groups []*RouterGroup // store all groups
+	}
+)
+
+func New() *Engine {
+	engine := &Engine{
+		router: newRouter(),
+	}
+	engine.RouterGroup = &RouterGroup{engine: engine}
+	engine.groups = []*RouterGroup{engine.RouterGroup}
+	return engine
+}
+
+// Group is defined to create a RouterGroup
+// remember all  groups share the same Engine interface
+func (group *RouterGroup) Group(prefix string) *RouterGroup {
+	engine := group.engine
+	newGroup := &RouterGroup{
+		prefix: group.prefix + prefix,
+		parent: group,
+		engine: engine,
+	}
+	engine.groups = append(engine.groups, newGroup)
+	return newGroup
+}
+
+func (group *RouterGroup) addRoute(method string, comp string, handler HandlerFunc) {
+	pattern := group.prefix + comp
+	group.engine.router.addRoute(method, pattern, handler)
+}
+
+// GET defines the method to add GET request
+func (group *RouterGroup) GET(pattern string, handler HandlerFunc) {
+	group.addRoute("GET", pattern, handler)
+}
+
+// POST defines the method to add POST request
+func (group *RouterGroup) POST(pattern string, handler HandlerFunc) {
+	group.addRoute("POST", pattern, handler)
+}
+
+func (group *RouterGroup) Use(middleware ...HandlerFunc) {
+	group.middlewares = append(group.middlewares, middleware...)
+}
+
+// RUN defines the method to start a http server
+func (engine *Engine) RUN(addr string) (err error) {
+	return http.ListenAndServe(addr, engine)
+}
+
+// 当接收到一个具体的请求时，要判断该请求适用于那些中间件，
+// 在这里简单的通过 URL 的前缀来判断。得到中间件列表后，
+// 赋值给 handlers
+func (engine *Engine) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	var middlewares []HandlerFunc
+	for _, group := range engine.groups {
+		if strings.HasPrefix(r.URL.Path, group.prefix) {
+			middlewares = append(middlewares, group.middlewares...)
+		}
+	}
+	c := newContent(w, r)
+	c.handlers = middlewares
+	engine.router.handle(c)
+}
---
+++ Gee/05_middleware/gee/router.go
@@ -0,0 +1,92 @@
+package gee
+
+import (
+	"log"
+	"net/http"
+	"strings"
+)
+
+type router struct {
+	roots    map[string]*node
+	handlers map[string]HandlerFunc
+}
+
+func newRouter() *router {
+	return &router{
+		roots:    make(map[string]*node),
+		handlers: make(map[string]HandlerFunc),
+	}
+}
+
+func getKey(method, pattern string) string {
+	return method + "-" + pattern
+}
+
+// only one * is allowed
+func parsePattern(pattern string) []string {
+	vs := strings.Split(pattern, "/")
+
+	parts := make([]string, 0)
+	for _, item := range vs {
+		if item != "" {
+			parts = append(parts, item)
+			if item[0] == '*' {
+				break
+			}
+		}
+	}
+	return parts
+}
+
+// 添加路由，规则pattern对应handler, 再往roots中插入结点
+func (r *router) addRoute(method string, pattern string, handler HandlerFunc) {
+	log.Printf("Route %4s - %s", method, pattern)
+	key := getKey(method, pattern)
+	parts := parsePattern(pattern)
+	if _, ok := r.roots[method]; !ok {
+		r.roots[method] = &node{}
+	}
+	r.roots[method].insert(pattern, parts, 0)
+	r.handlers[key] = handler
+}
+
+// 获取路由的结点node，并解析里面的参数
+func (r *router) getRoute(method, path string) (*node, map[string]string) {
+	searchParts := parsePattern(path) // 拆分路径转
+	params := make(map[string]string) // 路径中的参数
+	root, ok := r.roots[method]
+	if !ok {
+		return nil, nil
+	}
+	n := root.search(searchParts, 0) // 查找对应结点
+	if n != nil {
+		parts := parsePattern(n.pattern) // 解析路由规则
+		for index, part := range parts {
+			if part[0] == ':' {
+				params[part[1:]] = searchParts[index] // 从规则中找到可变参数，获取对应路径中的值
+			}
+			if part[0] == '*' && len(part) > 1 {
+				params[part[1:]] = strings.Join(searchParts[index:], "/")
+				break
+			}
+		}
+	}
+	return n, params
+}
+
+func (r *router) handle(c *Content) {
+	n, params := r.getRoute(c.Method, c.Path)
+	var curHandler HandlerFunc
+	if n != nil {
+		c.Params = params
+		key := getKey(c.Method, n.pattern)
+		// 把当前路由要执行的 handler 跟中间件一样放入 handlers 中
+		curHandler = r.handlers[key]
+	} else {
+		curHandler = func(c *Content) {
+			c.String(http.StatusNotFound, "404 Not Found path: %s", c.Path)
+		}
+	}
+	c.handlers = append(c.handlers, curHandler)
+	c.Next()
+}
