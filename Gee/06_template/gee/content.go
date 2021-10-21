7b8c82c812e0d5965e46ec1896a8b560677142cb
limitzhang <470299041@qq.com>
2021-10-21 22:01:46 +0800

    Day6 模板

--- Gee/06_template/gee/content.go
+++ Gee/06_template/gee/content.go
@@ -21,6 +21,8 @@ type Content struct {
 	// middleware
 	handlers []HandlerFunc
 	index    int
+	// engine pointer
+	engine *Engine
 }
 
 func newContent(w http.ResponseWriter, req *http.Request) *Content {
@@ -90,8 +92,10 @@ func (c *Content) Data(code int, data []byte) {
 	_, _ = c.Writer.Write(data)
 }
 
-func (c *Content) Html(code int, html string) {
+func (c *Content) Html(code int, name string, data interface{}) {
 	c.SetHeader("Content-Type", "text/html")
 	c.Status(code)
-	_, _ = c.Writer.Write([]byte(html))
+	if err := c.engine.htmlTemplates.ExecuteTemplate(c.Writer, name, data); err != nil {
+		c.Fail(500, err.Error())
+	}
 }
--- Gee/06_template/gee/gee.go
+++ Gee/06_template/gee/gee.go
@@ -1,6 +1,7 @@
 package gee
 
 import (
+	"html/template"
 	"net/http"
 	"path"
 	"strings"
@@ -19,8 +20,10 @@ type (
 	}
 	Engine struct {
 		*RouterGroup
-		router *router
-		groups []*RouterGroup // store all groups
+		router        *router
+		groups        []*RouterGroup     // store all groups
+		htmlTemplates *template.Template // for html render
+		funcMap       template.FuncMap   // for html render
 	}
 )
 
@@ -88,6 +91,14 @@ func (group *RouterGroup) Static(relativePath string, root string) {
 	group.GET(urlPattern, handler)
 }
 
+func (engine *Engine) SetFuncMap(funcMap template.FuncMap) {
+	engine.funcMap = funcMap
+}
+
+func (engine *Engine) LoadHTMLGlob(pattern string) {
+	engine.htmlTemplates = template.Must(template.New("").Funcs(engine.funcMap).ParseGlob(pattern))
+}
+
 // RUN defines the method to start a http server
 func (engine *Engine) RUN(addr string) (err error) {
 	return http.ListenAndServe(addr, engine)
@@ -105,5 +116,6 @@ func (engine *Engine) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 	c := newContent(w, r)
 	c.handlers = middlewares
+	c.engine = engine
 	engine.router.handle(c)
 }
