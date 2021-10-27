8c87833de2633bf5b35e29749cee6f81f1e9aef7
limitzhang <470299041@qq.com>
2021-10-27 16:47:09 +0800

    Day1 LRU缓存淘汰策略

--- gee-cache/01_lru/geecache/lru/lru_test.go
+++ gee-cache/01_lru/geecache/lru/lru_test.go
@@ -1,6 +1,7 @@
 package lru
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -34,5 +35,28 @@ func TestRemoveOldest(t *testing.T) {
 	lru.Add(k3, String(v3))
 	if _, ok := lru.Get(k1); ok || lru.Len() != 2 {
 		t.Fatalf("RemoveOldest key1 failed")
+	}
+}
+
+func TestOnEvicted(t *testing.T) {
+	k1, k2, k3 := "key1", "key2", "key3"
+	v1, v2, v3 := "value1", "value2", "value3"
+
+	keys := make([]string, 0)
+	callback := func(k string, v Value) {
+		keys = append(keys, k)
+	}
+
+	l := len(k1 + k2 + v1 + v2)
+	lru := New(int64(l), callback)
+	lru.Add(k1, String(v1))
+	lru.Add(k2, String(v2))
+	lru.Add(k3, String(v3))
+	if _, ok := lru.Get(k1); ok || lru.Len() != 2 {
+		t.Fatalf("RemoveOldest key1 failed")
+	}
+	expect := []string{"key1"}
+	if !reflect.DeepEqual(expect, keys) {
+		t.Fatalf("call Onevicted failed, expect keys equals to %s", expect)
 	}
 }
